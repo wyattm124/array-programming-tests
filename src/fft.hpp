13cfbc7173d0af8cc7b53298490230ba1401eecd
Wyatt <wyatt.maleta@gamil.com>
2025-08-14 13:25:19 -0700

    add fft.hpp with full spilt radix cooley tookey

--- src/fft.hpp
+++ src/fft.hpp
@@ -7,34 +7,36 @@
 #include <iostream>
 
 /// TODO: 
-/// (1) - Raders DFT implementations for 3 - 7
-/// (2) - specialized DFT implementations for 2, 4, 6, 8
-/// (3) - implement these as base cases in FFTPlan
-/// (4) - implemnt them for recursive steps in FFT plan
-/// (5) - implement Bluestiens's for numbers that do not decompse into these factors
-/// (6) - implement a top level search for best number to pad an input to for FFT
+/// (1) - specialized DFT implementations for 6, 7, 8 as base cases
+///   -> these should be able to happen in registers and be very fast!
+/// (2) - implement Bluestiens's for numbers that do not decompse into these factors
+/// (3) - implement a top level search for best number to pad an input to for FFT
 
 // For LLVM MCA Analysis
 #define MCA_START __asm volatile("# LLVM-MCA-BEGIN");
 #define MCA_END __asm volatile("# LLVM-MCA-END");
 
 namespace FFT {
-    // Neon intrinsics
-    inline float32x2_t neon_mul(const float32x2_t &__restrict__ a, const float32x2_t &__restrict__ b) noexcept {
-        return {a[0] * b[0] - a[1] * b[1], a[0] * b[1] + a[1] * b[0]};
-    }
+    // Neon intrinsics 
     inline float neon_abs(const float32x2_t &__restrict__ a) noexcept {
         return std::sqrt(a[0]*a[0] + a[1]*a[1]);
     }
     inline float32x2_t neon_conj(const float32x2_t &__restrict__ a) noexcept {
         return {a[0], -a[1]};
     }
+    inline float32x2_t neon_mul(const float32x2_t &__restrict__ a, const float32x2_t &__restrict__ b) noexcept {
+        return {a[0] * b[0] - a[1] * b[1], a[0] * b[1] + a[1] * b[0]};
+    }
+    inline float32x2_t neon_mul_conj(const float32x2_t &__restrict__ a, const float32x2_t &__restrict__ b) noexcept {
+        return {a[0] * b[0] + a[1] * b[1], a[0] * b[1] - a[1] * b[0]};
+    }
 
     // For DFT Transpositions
     template <unsigned int A, unsigned int B, typename T>
     void DFT_binner(T *__restrict__ in, T *__restrict__ out) noexcept {
         // Base case
-        if constexpr (A * B == 1) {
+        if constexpr (A == 1 || B == 1) {
+            std::copy(in, in + A * B, out);
             return;
         }
 
@@ -55,7 +57,7 @@ namespace FFT {
     template <unsigned int A, unsigned int B>
     void DFT_binner_inplace(float32x2_t *__restrict__ data) {
         // Base case
-        if constexpr (A < 2) {
+        if constexpr (A == 1 || B == 1) {
             return;
         }
 
@@ -101,26 +103,13 @@ namespace FFT {
     }
     
     constexpr double TwoPI = 2.0 * M_PI;
-    constexpr double NegTwoPI = -TwoPI;
-    
-    // TODO: Only for circular convolution test! everything 7 seems to all be able to
-    //  happen in registers and is sub ns fast!
-    void raders_7(float32x2_t *__restrict__ in, float32x2_t *__restrict__ out, float32x2_t *__restrict__ coeff) {
-        MCA_START
-        //#pragma clang loop unroll_count(7)
-        for (unsigned int i = 0; i < 7; i++)
-            //#pragma clang loop unroll_count(7)
-            for (unsigned int j = 0; j < 7; j++)
-                out[i] += neon_mul(in[j], coeff[(j + i) % 7]);
-        MCA_END
-        return;   
-    }
+    constexpr double NegTwoPI = -TwoPI; 
 
-    template <std::size_t N>
+    template <unsigned int N>
     class FFTPlan {
         public:
-        static constexpr std::size_t A = prime_factor::get_prime_factor(N);
-        static constexpr std::size_t B = N / A;
+        static constexpr unsigned int B = prime_factor::get_prime_factor(N);
+        static constexpr unsigned int A = N / B;
 
         FFTPlan() {
             // Ensure the coeffs are calculated
@@ -156,57 +145,96 @@ namespace FFT {
         
         // In-place Mixed Radix Cooley Tukey FFT
         static void fft_recurse(float32x2_t *__restrict__ data) noexcept {
-            // Base case
-            if constexpr (N < 2) {
-                return;
+            // Base case + direct DFT cases
+            if constexpr (N == 1) {
+                // Do nothing, just return
             } else if constexpr (N == 2) {
-                const float32x2_t a = data[0] + data[1];
+                const auto temp = data[0] + data[1];
                 data[1] = data[0] - data[1];
-                data[0] = a;
-                return;
-            }
-
-            std::array<float32x2_t, N> temp;
-            if constexpr (B == 1) {
-                for (std::size_t i = 0; i < A; i++)
-                    temp[i] = data[i];
-            } else {
-                // Transpose Input Data around the radix
-                DFT_binner<B, A>(data, temp.data());
-
-                // Recursively apply fft to each bin
-                for (std::size_t i = 0; i < A; i++)
-                    FFTPlan<B>::fft_recurse(temp.data() + (i * B));
-            }
-
-            // Do tensor multiplication of coeffs with data
-            float32x2_t* coefs = gen_coefs_by_angle();
-            for (std::size_t i = 0; i < B; i++) {
-                #pragma clang loop unroll_count(32)
-                for (std::size_t j = 0; j < A; j++) {
-                    //MCA_START
-                    float32x2_t temp_ans = {0, 0};
-                    auto index = i * A * A + j * A;
-                    #pragma clang loop unroll_count(32)
-                    for (std::size_t k = 0; k < A; k++) {
-                        const auto coeff = coefs[index + k];
-                        if constexpr (B == 1) {
-                            const auto temp_val = temp[k];
-                            temp_ans += neon_mul(temp_val, coeff);
-                        } else {
-                            const auto temp_val = temp[i + (k * B)];
-                            temp_ans += neon_mul(temp_val, coeff);
-                        }
-                    }
-
-                    // Store the answer
-                    if constexpr (B == 1) {
-                        data[j] = temp_ans;
-                    } else {
-                        data[i + (j * B)] = temp_ans;
-                    }
-                    //MCA_END
+                data[0] = temp;
+            } else if constexpr (N == 3) {
+                // TODO : write all math explicitly? See if that gives a speed boost.
+                static constexpr float32x2_t third_root = {-0.5f, 0.86660254f};
+                const float32x2_t x_0 = data[0];
+                const float32x2_t x_1 = data[1];
+                const float32x2_t x_2 = data[2];
+                data[0] = x_0 + x_1 + x_2;
+                data[1] = x_0 + neon_mul_conj(third_root, x_1) + neon_mul(third_root, x_2);
+                data[2] = x_0 + neon_mul(third_root, x_1) + neon_mul_conj(third_root, x_2);
+            } else if constexpr (N == 4) {
+                const float32x2_t x_0 = data[0];
+                const float32x2_t x_1 = data[1];
+                const float32x2_t x_2 = data[2];
+                const float32x2_t x_3 = data[3];
+                { // First do evens
+                    const float32x2_t a = x_0 + x_2;
+                    const float32x2_t b = x_1 + x_3;
+                    data[0] = a + b;
+                    data[2] = a - b;
                 }
+                { // Then do odds
+                    const float32x2_t a = x_0 - x_2;
+                    const float32x2_t temp_b = x_1 - x_3;
+                    const float32x2_t b = {temp_b[1], -temp_b[0]};
+                    data[1] = a + b;
+                    data[3] = a - b;
+                }
+            } else if constexpr (N == 5) {
+                static constexpr float32x2_t root_1 = {0.30901699, 0.95105652};
+                static constexpr float32x2_t root_2 = {-0.80901699, 0.58778525};
+                const float32x2_t x_0 = data[0];
+                const float32x2_t x_1 = data[1];
+                const float32x2_t x_2 = data[2];
+                const float32x2_t x_3 = data[3];
+                const float32x2_t x_4 = data[4];
+                data[0] = x_0 + x_1 + x_2 + x_3 + x_4;
+                data[1] = x_0 + neon_mul_conj(root_1, x_1) + neon_mul_conj(root_2, x_2) +
+                               neon_mul(root_2, x_3) + neon_mul(root_1, x_4);
+                data[2] = x_0 + neon_mul_conj(root_2, x_1) + neon_mul(root_1, x_2) +
+                               neon_mul_conj(root_1, x_3) + neon_mul(root_2, x_4);
+                data[3] = x_0 + neon_mul(root_2, x_1) + neon_mul_conj(root_1, x_2) +
+                               neon_mul(root_1, x_3) + neon_mul_conj(root_2, x_4);
+                data[4] = x_0 + neon_mul(root_1, x_1) + neon_mul(root_2, x_2) +
+                               neon_mul_conj(root_2, x_3) + neon_mul_conj(root_1, x_4);
+            } else if constexpr (A == 1 || B == 1) { // Do the full DFT
+                auto dft_matrix = gen_coefs_by_angle();
+                std::array<float32x2_t, N> temp_data;
+
+                // Do a full DFT
+                for (unsigned int i = 0; i < N; i++) {
+                    float32x2_t temp_ans = {0, 0};
+                    for (unsigned int j = 0; j < N; j++) {
+                        temp_ans += neon_mul(data[j], dft_matrix[i * N + j]);
+                    }
+                    temp_data[i] = temp_ans;
+                }
+
+                for (unsigned int i = 0; i < N; i++)
+                    data[i] = temp_data[i];
+            } else { // Do a mixed radix FFT
+                std::array<float32x2_t, N> temp_data;
+
+                // Transpose Input Data around the first radix
+                DFT_binner<A, B>(data, temp_data.data());
+ 
+                // Do the A sized FFT on each bin
+                for (unsigned int i = 0; i < B; i++)
+                    FFTPlan<A>::fft_recurse(temp_data.data() + (i * A));
+
+                // Multiply by Cooley Tukey Twiddle factors
+                auto twiddle_factors = gen_coefs_by_angle();
+                for (unsigned int i = A; i < N; i++) {
+                    temp_data[i] = neon_mul(temp_data[i], twiddle_factors[((i / A) * (i % A)) % N]);
+                }
+
+                // Transpose around the second radix
+                DFT_binner<B, A>(temp_data.data(), data);
+
+                // Do the B sized FFT on each bin
+                for (unsigned int i = 0; i < A; i++)
+                    FFTPlan<B>::fft_recurse(data + (i * B));
+
+                DFT_binner_inplace<A, B>(data);
             }
 
             // This function is in-place! the input is modified with the result.
@@ -258,21 +286,30 @@ namespace FFT {
         };
         static float32x2_t* gen_coefs_by_angle() {
             static float32x2_t* coefs = []{
-                float32x2_t* result = new float32x2_t[N * A];
-            
-                for (std::size_t i = 0; i < B; i++) {
-                    for (std::size_t j = 0; j < A; j++) {
-                        for (std::size_t k = 0; k < A; k++) {
-                            const auto index = i * A * A + j * A + k;
-                            result[index] = gen_factor_by_angle(k, j, i);
-                        }
-                    }
+              if constexpr (A == 1 || B == 1) { // DFT matrix
+                float32x2_t* result = new float32x2_t[N * N];
+                for (std::size_t i = 0; i < N; i++) {
+                  for (std::size_t j = 0; j < N; j++) {
+                    const double angle = 
+                      NegTwoPI * static_cast<double>((i * j) % N) / static_cast<double>(N);
+                        result[i * N + j] = float32x2_t{static_cast<float>(std::cos(angle)),
+                                                        static_cast<float>(std::sin(angle))};
+                  }
                 }
                 return result;
+              } else { // Split Radix FFT Twiddle factors
+                float32x2_t* result = new float32x2_t[N];
+                for (std::size_t i = 0; i < N; i++) {
+                  const double angle = 
+                      NegTwoPI * static_cast<double>(i) / static_cast<double>(N);
+                  result[i] = {static_cast<float>(std::cos(angle)), static_cast<float>(std::sin(angle))};
+                }
+                return result;
+              }
             }();
             return coefs;
         } 
-    };
+    }; 
 
     template <std::size_t N>
     struct FFTPlanBasic {
@@ -340,9 +377,10 @@ namespace FFT {
             // This function is in-place! the input is modified with the result.
             return;
         }
-    }; 
+    };
 
-    constexpr void wave_gen(std::complex<float> *time_domain, std::complex<float> *freq_domain,
+    template <typename T>
+    constexpr void wave_gen(T *time_domain, T *freq_domain,
         std::size_t N,
         unsigned int f = 1,
         unsigned int phase = 0,
@@ -350,45 +388,34 @@ namespace FFT {
         for (unsigned int i = 0; i < N; i++) {
             const float angle = static_cast<float>(TwoPI) * static_cast<float>((i * f) + phase)
                                   / static_cast<float>(N);
-            time_domain[i] += std::complex<float>{std::cosf(angle), std::sinf(angle)} * static_cast<float>(amp);
+            time_domain[i] += T{std::cosf(angle), std::sinf(angle)} * static_cast<float>(amp);
         }
         const float angle = static_cast<float>(TwoPI) * 
                                   (static_cast<float>(phase) / static_cast<float>(N));
-        freq_domain[f] += std::complex<float>{std::cosf(angle), std::sinf(angle)} * static_cast<float>(amp);
-    }
+        freq_domain[f] += T{std::cosf(angle), std::sinf(angle)} * static_cast<float>(amp);
+    } 
 
-    constexpr void wave_gen(float32x2_t *time_domain, float32x2_t *freq_domain,
-        std::size_t N,
-        unsigned int f = 1,
-        unsigned int phase = 0,
-        unsigned int amp = 1) {
-        for (unsigned int i = 0; i < N; i++) {
-            const float angle = static_cast<float>(TwoPI) * static_cast<float>((i * f) + phase)
-                                  / static_cast<float>(N);
-            time_domain[i] += float32x2_t{std::cosf(angle), std::sinf(angle)} * static_cast<float>(amp);
-        }
-        const float angle = static_cast<float>(TwoPI) * 
-                                  (static_cast<float>(phase) / static_cast<float>(N));
-        freq_domain[f] += float32x2_t{std::cosf(angle), std::sinf(angle)} * static_cast<float>(amp);
-    }
-
-    constexpr void wave_gen_lcg(std::complex<float> *time_domain, std::complex<float> *freq_domain,
-        std::size_t N) {
-        for (unsigned int i = 0; i < 13; i++) {
+    template <typename T>
+    constexpr void wave_gen_lcg(T *time_domain, T *freq_domain, unsigned int N) {
+        if (N < 13) {
+            if (N > 7) {
+                wave_gen(time_domain, freq_domain, N,
+                    7, 2, 1);
+            }
+            if (N > 3) {
+                wave_gen(time_domain, freq_domain, N,
+                    3, 3, 1);
+            }
             wave_gen(time_domain, freq_domain, N,
-                ((i + 7) * 3) % N,
-                ((i + 5) * 11) % N,
-                ((i + 11) * 13) % N);
-        }
-    }
-
-    constexpr void wave_gen_lcg(float32x2_t *time_domain, float32x2_t *freq_domain,
-        std::size_t N) {
-        for (unsigned int i = 0; i < 13; i++) {
-            wave_gen(time_domain, freq_domain, N,
-                ((i + 7) * 3) % N,
-                ((i + 5) * 11) % N,
-                ((i + 11) * 13) % N);
+                1, 1, 1);
+            return;
+        } else {
+            for (unsigned int i = 0; i < 13; i++) {
+                wave_gen(time_domain, freq_domain, N,
+                    ((i + 7) * 3) % N,
+                    ((i + 5) * 11) % N,
+                    ((i + 11) * 13) % N);
+            }
         }
     }
 }
